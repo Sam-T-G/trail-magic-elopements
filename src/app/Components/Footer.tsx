61f99431d4f2d8a3b5dfa1e3c779675b62fcc201
Sam.G <samuelgerungan@gmail.com>
2024-03-14 18:43:08 -0700

    Update Footer.tsx

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="wrapper flex flex-wrap">
-			<div>Footer</div>
-		</div>
+		<footer className="text-gray-600">
+			<div className="container px-5 py-8 mx-auto flex flex-col justify-between items-center sm:flex-row">
+				<a className="flex title-font font-medium items-center md:justify-start justify-center">
+					Footer Content
+				</a>
+			</div>
+		</footer>
 	);
 };
 
