01993c58cee2544f2a0352c83e822a0e8c915da8
Sam.G <samuelgerungan@gmail.com>
2024-03-14 19:01:43 -0700

    Update Navbar.tsx

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -3,17 +3,23 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<header className="">
-			<div className="">
-				<a className="">
-					<div>Logo</div>
-					<h1>Trail Magic Elopements</h1>
+		<header className="text-gray-600">
+			<div className="container mx-auto flex flex-wrap p5 md:flex-row items-center justify-between">
+				<a className="flex font-medium items-center md:mb-0">
+					<div className="">Logo</div>
+					<h1 className="">Trail Magic Elopements</h1>
 				</a>
 			</div>
-			<div className="">
-				<nav className="">
+			<div className="flex flex-wrap items-center">
+				<nav className="md:ml-auto flex flex-wrap items-center justify-center">
 					<Link href="/">
 						<span className="">Home</span>
+					</Link>
+					<Link href="/">
+						<span className="">About</span>
+					</Link>
+					<Link href="/">
+						<span className="">Pricing</span>
 					</Link>
 				</nav>
 			</div>
