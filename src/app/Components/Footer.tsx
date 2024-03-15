5315d0bb2cdc48715dd43da9f0280972321b5f47
Sam.G <samuelgerungan@gmail.com>
2024-03-14 19:37:45 -0700

    Update Footer.tsx

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
 	return (
@@ -7,6 +8,30 @@ const Footer = () => {
 				<a className="flex title-font font-medium items-center md:justify-start justify-center">
 					Footer Content
 				</a>
+				<div className="flex flex-wrap items-center">
+					<nav className="md:ml-auto flex flex-wrap items-center justify-center">
+						<Link href="/">
+							<span className="flex mr-5 transform transition-transform hover:scale-110 cursor-pointer">
+								Home
+							</span>
+						</Link>
+						<Link href="/about">
+							<span className="flex mr-5 transform transition-transform hover:scale-110 cursor-pointer">
+								About
+							</span>
+						</Link>
+						<Link href="/contact">
+							<span className="flex mr-5 transform transition-transform hover:scale-110 cursor-pointer">
+								Pricing
+							</span>
+						</Link>
+					</nav>
+					<Link href="/projects">
+						<button className="inline-flex items-center border-indigo-600 border-2 bg-white text-indigo-600 font-semibold py-1 px-3 focus:outline-none hover:bg-indigo-600 transform transition-transform hover:scale-110 hover:text-white rounded-lg text-base">
+							Contact
+						</button>
+					</Link>
+				</div>
 			</div>
 		</footer>
 	);
