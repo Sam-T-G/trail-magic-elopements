41b5fb755950fcef1011c450636e17b5889af0d3
Sam.G <samuelgerungan@gmail.com>
2024-03-14 18:18:57 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-	return <div></div>;
+	return (
+		<div className="container flex flex-wrap">
+			<div className="w-vw h-vh">Main Landing Page Here</div>
+		</div>
+	);
 };
 
 export default App;
