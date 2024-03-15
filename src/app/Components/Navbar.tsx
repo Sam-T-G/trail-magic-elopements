58a3f094128b00d60951c4468458a632930697a5
Sam.G <samuelgerungan@gmail.com>
2024-03-14 18:38:02 -0700

    Navbar Creation

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<header>
+			<div>
+				<a>
+					<div>Logo</div>
+					<h1>Trail Magic Elopements</h1>
+				</a>
+			</div>
+			<div>
+				<nav>
+					<Link href="/">
+						<span className="">Home</span>
+					</Link>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Navbar;
