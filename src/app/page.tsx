4157546576834f4923dd4a7c3e30492b87aec06d
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-18 16:29:29 -0700

    Page spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ const App = () => {
 					className="w-auto h-auto bg-cover bg-center flex justify-center relative"
 					style={{
 						backgroundImage: "url('/tme-hero.jpg')",
-						minWidth: "100dvw",
-						minHeight: "100dvh",
+						minWidth: "100vw",
+						minHeight: "100vh",
 					}}>
 					<div className="absolute inset-0 flex flex-col justify-center items-center">
 						<h1 className="text-4xl text-white font-bold">
@@ -23,7 +23,7 @@ const App = () => {
 				</div>
 			</div>
 			<div className="container flex flex-col p-0 m-0 justify-center">
-				<div className="width-dvw font-semibold text-4xl p-0 m-0 flex justify-center">
+				<div className="width-dvw font-semibold text-4xl p-1 m-5 flex justify-center">
 					Trail Magic
 				</div>
 			</div>
