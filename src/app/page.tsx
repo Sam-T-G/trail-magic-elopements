663c66a988b29a636ba43b007661fc32c771f284
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-18 14:56:03 -0700

    Hero Initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Image from "next/image";
 
 const App = () => {
 	return (
 		<div className="container flex flex-wrap">
-			<div className="w-auto h-auto">
-				<div className="">
-					Hero Title
-					<div className="">Hero Subtitle</div>
+			<div
+				className="w-full h-screen bg-cover bg-center relative"
+				style={{ backgroundImage: "url('/tme-hero.jpg')" }}>
+				<div className="absolute inset-0 flex flex-col justify-center items-center">
+					<h1 className="text-4xl text-white font-bold">Hero Title</h1>
+					<p className="text-xl text-white">Hero Subtitle</p>
 				</div>
 			</div>
 		</div>
