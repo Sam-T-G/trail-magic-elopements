b97e30335d9e3b87980f1aa3f741b93e120d7dd8
Sam.G <samuelgerungan@gmail.com>
2024-03-22 22:31:43 -0700

    Update ZoomParallax.tsx

--- src/app/Components/ZoomParallax.tsx
+++ src/app/Components/ZoomParallax.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import Picture1 from "../../../public/tme-kt-1.jpg";
+import Picture2 from "../../../public/tme-kt-2.jpg";
+import Picture3 from "../../../public/tme-kt-3.jpg";
+import Picture4 from "../../../public/tme-kt-4.jpg";
 
 const ZoomParallax = () => {
-	return (
-		<main>
-			<ZoomParallax />
-		</main>
-	);
+	return <main></main>;
 };
 
 export default ZoomParallax;
