6f777457af313253e068fc1a01f36ce028f2457d
Sam.G <samuelgerungan@gmail.com>
2024-03-22 23:01:54 -0700

    Update ZoomParallax.tsx

--- src/app/Components/ZoomParallax.tsx
+++ src/app/Components/ZoomParallax.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
-import { useScroll } from "framer-motion";
+import { useScroll, useTransform } from "framer-motion";
 import Picture1 from "../../../public/tme-kt-1.jpg";
 import Picture2 from "../../../public/tme-kt-2.jpg";
 import Picture3 from "../../../public/tme-kt-3.jpg";
@@ -8,9 +8,15 @@ import Picture4 from "../../../public/tme-kt-4.jpg";
 
 const ZoomParallax = () => {
 	const container = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ["start start", "end end"],
+	});
+
+	const scale4 = useTransform(scrollYProgress, [0, 1], [0, 4]);
 
 	return (
-		<div className="h-[300vh] relative w-screen">
+		<div ref={container} className="h-[300vh] relative w-screen">
 			<div className="sticky top-0 my-[50vh] h-screen bg-orange-500 w-screen">
 				<div className="w-full h-full absolute top-0 flex items-center justify-center">
 					<div className="w-[25vw] h-[25vh] relative">
