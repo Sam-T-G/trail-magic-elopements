8c2ce1155eb05a0ca7046c738b533f2204df8994
Sam.G <samuelgerungan@gmail.com>
2024-03-22 22:31:58 -0700

    Update ZoomParallax.tsx

--- src/app/Components/ZoomParallax.tsx
+++ src/app/Components/ZoomParallax.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import Picture1 from "../../../public/tme-kt-1.jpg";
 import Picture2 from "../../../public/tme-kt-2.jpg";
 import Picture3 from "../../../public/tme-kt-3.jpg";
