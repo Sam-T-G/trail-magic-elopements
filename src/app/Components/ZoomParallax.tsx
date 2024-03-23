48d90c6b2177f20ca5176e101dd048a1bca014dc
Sam.G <samuelgerungan@gmail.com>
2024-03-22 22:54:51 -0700

    Update ZoomParallax.tsx

--- src/app/Components/ZoomParallax.tsx
+++ src/app/Components/ZoomParallax.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import { useScroll } from "framer-motion";
 import Picture1 from "../../../public/tme-kt-1.jpg";
 import Picture2 from "../../../public/tme-kt-2.jpg";
 import Picture3 from "../../../public/tme-kt-3.jpg";
 import Picture4 from "../../../public/tme-kt-4.jpg";
 
 const ZoomParallax = () => {
+	const container = useRef(null);
+
 	return (
 		<div className="h-[300vh] relative w-screen">
 			<div className="sticky top-0 my-[50vh] h-screen bg-orange-500 w-screen">
