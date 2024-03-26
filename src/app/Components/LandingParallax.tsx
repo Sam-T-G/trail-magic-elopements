ec69611847a5abb261e1b0c9e6941d3b62a3b859
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-26 16:45:34 -0700

    Update LandingParallax.tsx

--- src/app/Components/LandingParallax.tsx
+++ src/app/Components/LandingParallax.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+"use client";
+import { useRef } from "react";
+import styles from "../../app/page.module.scss";
+import Picture1 from "../../../public/tme-kt-1.jpg";
+import Picture2 from "../../../public/tme-kt-2.jpg";
+import Picture3 from "../../../public/tme-kt-3.jpg";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const LandingParallax = () => {
 	return <div>LandingParallax</div>;
