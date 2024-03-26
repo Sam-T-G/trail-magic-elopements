2d6ba77ea07b16d4e408c05aff8f6243d5eee285
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-26 16:46:07 -0700

    Update LandingParallax.tsx

--- src/app/Components/LandingParallax.tsx
+++ src/app/Components/LandingParallax.tsx
@@ -8,6 +8,15 @@ import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const LandingParallax = () => {
+	const container = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ["start end", "end start"],
+	});
+	const sm = useTransform(scrollYProgress, [0, 1], [0, -50]);
+	const md = useTransform(scrollYProgress, [0, 1], [0, -150]);
+	const lg = useTransform(scrollYProgress, [0, 1], [0, -250]);
+
 	return <div>LandingParallax</div>;
 };
 
