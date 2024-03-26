9199455a7835e17fbca5f3e45eaa1c1fd654206e
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-26 16:49:55 -0700

    Update LandingParallax.tsx

--- src/app/Components/LandingParallax.tsx
+++ src/app/Components/LandingParallax.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRef } from "react";
-import styles from "../../app/page.module.scss";
 import Picture1 from "../../../public/tme-kt-1.jpg";
 import Picture2 from "../../../public/tme-kt-2.jpg";
 import Picture3 from "../../../public/tme-kt-3.jpg";
@@ -17,7 +16,57 @@ const LandingParallax = () => {
 	const md = useTransform(scrollYProgress, [0, 1], [0, -150]);
 	const lg = useTransform(scrollYProgress, [0, 1], [0, -250]);
 
-	return <div>LandingParallax</div>;
+	const images = [
+		{
+			src: Picture1,
+			y: 0,
+		},
+		{
+			src: Picture2,
+			y: lg,
+		},
+		{
+			src: Picture3,
+			y: md,
+		},
+	];
+
+	return (
+		<div ref={container} className={styles.container}>
+			<div className={styles.body}>
+				<motion.h1 style={{ y: sm }}>Parallax</motion.h1>
+				<h1>Scroll</h1>
+				<div className={styles.word}>
+					<p>
+						{word.split("").map((letter, i) => {
+							const y = useTransform(
+								scrollYProgress,
+								[0, 1],
+								[0, Math.floor(Math.random() * -75) - 25]
+							);
+							return (
+								<motion.span style={{ top: y }} key={`l_${i}`}>
+									{letter}
+								</motion.span>
+							);
+						})}
+					</p>
+				</div>
+			</div>
+			<div className={styles.images}>
+				{images.map(({ src, y }, i) => {
+					return (
+						<motion.div
+							style={{ y }}
+							key={`i_${i}`}
+							className={styles.imageContainer}>
+							<Image src={src} placeholder="blur" alt="image" fill />
+						</motion.div>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default LandingParallax;
